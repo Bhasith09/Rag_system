ac16064e0be8ec22c56dfc5d9641e31b722851c3
Bhasith09 <bhasith009@gmail.com>
2026-05-04 14:09:26 +0300

    11 push to ci

--- backend/vectorstore.py
+++ backend/vectorstore.py
@@ -6,7 +6,7 @@ import uuid
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-client = chromadb.PersistentClient(path="./chroma_db")
+client = chromadb.EphemeralClient(path="./chroma_db")
 collection = client.get_or_create_collection(name="rag_docs")
 
 

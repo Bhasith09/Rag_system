dd17ffbd925acec87a2b66fe75529fc154df2615
Bhasith09 <bhasith009@gmail.com>
2026-05-10 16:58:53 +0300

    readme update img

--- backend/reranker.py
+++ backend/reranker.py
@@ -1,50 +1,29 @@
+# reranker.py
+
 from sentence_transformers import CrossEncoder
 
-reranker_model = CrossEncoder(
-    'cross-encoder/ms-marco-MiniLM-L-6-v2'
-)
+# Initialize the cross-encoder model
 
+reranker_model = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
 
-def rerank(query, documents, top_k=5):
-
+def rerank(query,documents, top_k=5):
     if not documents:
         return []
+    
+    #create (query, doc) pairs
+    pairs=[(query,doc) for doc in documents]
 
-    cleaned_docs = []
+    #get scores
+    scores=reranker_model.predict(pairs)
 
-    # Ensure every document is dictionary format
-    for doc in documents:
+    #combine docs + scores
+    scored_docs=list(zip(documents, scores))
 
-        if isinstance(doc, str):
+    #sort by score (high to low)
 
-            cleaned_docs.append({
-                "text": doc,
-                "page": "N/A",
-                "paragraph": "N/A",
-                "source": "Unknown"
-            })
+    ranked_docs=sorted(scored_docs,key=lambda x:x[1], reverse=True)
 
-        else:
-            cleaned_docs.append(doc)
 
-    # Create (query, text) pairs
-    pairs = [
-        (query, doc["text"])
-        for doc in cleaned_docs
-    ]
+    #returnonly docs
 
-    # Predict relevance scores
-    scores = reranker_model.predict(pairs)
-
-    # Combine docs + scores
-    scored_docs = list(zip(cleaned_docs, scores))
-
-    # Sort descending
-    ranked_docs = sorted(
-        scored_docs,
-        key=lambda x: x[1],
-        reverse=True
-    )
-
-    # Return top documents
     return [doc for doc, _ in ranked_docs[:top_k]]

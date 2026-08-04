64042703fba7c6e21636d46cc82274a1f01f3833
Bhasith <bhasith009@gmail.com>
2026-08-04 21:58:51 +0300

    api url from 127.0.0.1:8000 to backend:8000

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
-
-API_URL = "http://127.0.0.1:8000"
+API_URL = "http://backend:8000"
+#API_URL = "http://127.0.0.1:8000"
 
 st.set_page_config(page_title="Production Hybrid RAG", layout="wide")
 st.title("🚀 Production Hybrid RAG System")
